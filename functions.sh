39cb263bdae94193226711e14b18c42cd544a013
francisco.calle <francisco.calle.next@bbva.com>
2020-03-13 12:28:08 +0100

    refactor(functions): clear docker removes vols/networks

--- functions.sh
+++ functions.sh
@@ -7,3 +7,24 @@ clearDockerLog() {
   screen -S dockerlogdelete -p 0 -X stuff $'\n'
   screen -S dockerlogdelete -X quit
 }
+
+clearDocker() {
+  printf "🔥 🔥 🔥 \e[93m\nWARNING: This command will remove EVERY\n"
+  printf "  * container\n"
+  printf "  * image\n"
+  printf "  * volume\n"
+  printf "  * network\n"
+  printf "====================================================\n\e[0m"
+  printf "Type \e[91myes\e[0m to continue with the process: \e[2m"
+  read CONTINUE
+  printf "\e[0m"
+  if [ "$CONTINUE" != "yes" ]; then
+    echo "Reset has been canceled as per your request"
+    exit 0
+  fi
+  docker stop $(docker ps -a -q)
+  docker rm -f $(docker ps -a -q)
+  docker rmi $(docker images -q) --force
+  docker network rm $(docker network ls -q)
+  docker volume rm $(docker volume ls -q)
+}
