34a043302aeba51f28b1630b5db4b0b4ca48354c
francisco.calle <francisco.calle.next@bbva.com>
2020-03-13 12:27:17 +0100

    refactor: remove bbva next aliases

--- aliases.sh
+++ aliases.sh
@@ -3,12 +3,3 @@ alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall F
 alias wget='wget --no-check-certificate'
 alias resetSpotlight='sudo mdutil -a -i off;sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo mdutil -a -i on'
 
-# SERVICES
-alias redis-start='redis-server /usr/local/etc/redis.conf'
-alias mongo-start='mongod --config /usr/local/etc/mongod.conf'
-
-# BEEVA
-alias token-arq='anwbis -p arquitectura -e dev -r developer -b firefox --get_session'
-
-# OPENWEB
-alias anwbis-openweb-dev='anwbis -p webpublicas -e pro -r developer --profile default'
