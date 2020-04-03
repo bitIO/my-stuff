cb72879a007aa60db746894d6fc4aa13ec9cb7bc
Francisco Calle <francisco.calle.next@bbva.com>
2020-04-03 19:46:15 +0200

    feat(aliases): add brewup alias

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,7 @@
+# OSX aliases
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 alias wget='wget --no-check-certificate'
 alias resetSpotlight='sudo mdutil -a -i off;sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist;sudo mdutil -a -i on'
 
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
